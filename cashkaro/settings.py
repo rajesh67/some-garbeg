8c557e99ec866c63ab3cd2dde48052a706e6668e
rajesh meena <rajeshmeena.iitkgp@gmail.com>
2014-03-11 03:51:44 +0530

    cashkaro

--- cashkaro/settings.py
+++ cashkaro/settings.py
@@ -71,20 +71,20 @@ WSGI_APPLICATION = 'cashkaro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+#import dj_database_url
+#DATABASES['default'] =  dj_database_url.config()
  
 
-#DATABASES = {
-#    'default': {
- #       'ENGINE':'django.db.backends.postgresql_psycopg2',
- #       'NAME': 'mydb',
-  #      'USER': 'myuser',
-   #     'PASSWORD': '11cs30025',
-    #    'HOST': 'localhost',
-     #   'PORT': '',
- #   }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': '11cs30025',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
