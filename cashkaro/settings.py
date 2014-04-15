7d67a1877a6617b6667fe974ad0a6b10f7341f40
rajesh67 <rajeshmeena.iitkgp@gmail.com>
2014-04-15 12:01:00 +0530

    template

--- cashkaro/settings.py
+++ cashkaro/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'h94tmkt_@ibr*not9r6nyfzpsu3hgyxf-3v-&j8acyc8+mpc%('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-TEMPLATE_DEBUG = False
+TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
